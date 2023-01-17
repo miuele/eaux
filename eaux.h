1fe9d231b18b7e44f2ed94c9e6ba8f574e30a11d
Yoshiki Oka <urandq@gmail.com>
2023-01-17 11:18:04 +0900

    initial commit

---
+++ eaux.h
@@ -0,0 +1,39 @@
+#ifndef EAUX_H_INCLUDED
+#define EAUX_H_INCLUDED
+
+namespace eaux {
+
+namespace numbers {
+
+template <class T>
+T pi_v = 3.141592653589793238462643383279502884L;
+
+}
+
+constexpr float lerp(float a, float b, float t) noexcept {
+	return a + t * (b - a);
+}
+
+template <class T>
+constexpr const T &clamp(const T &v, const T &lo, const T &hi) {
+    return v < lo ? lo : hi < v ? hi : v; 
+}
+
+template <class Rep, class Period>
+constexpr float duration_to_float(const std::chrono::duration<Rep, Period> &d) {
+    return std::chrono::duration<float>(d).count();
+}
+
+template <class T, class ...Args>
+T *construct_at(T *p, Args &&...args) {
+	return ::new (const_cast<void *>(static_cast<const volatile void *>(p))) T(std::forward<Args>(args)...);
+}
+
+template <class T>
+void destroy_at(T *p) {
+	p->~T();
+}
+
+}
+
+#endif
